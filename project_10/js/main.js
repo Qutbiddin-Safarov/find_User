a795f436a35652a8db2896c759fb18fa59da1375
Qutbiddin-Safarov <qutbiddins@icloud.com>
2022-12-12 18:26:41 +0300

    main

--- project_10/js/main.js
+++ project_10/js/main.js
@@ -1,5 +1,7 @@
 const tbody = document.querySelector('#tbody');
 const pNameInput = document.querySelector('#p_name');
+const pPriceInput = document.querySelector('#p_price');
+const pWeightInput = document.querySelector('#p_weight');
 
 let fruits = [
     { product_name: "Apple", product_price: 2.1, product_weight: 320 },
@@ -13,11 +15,20 @@ let fruits = [
     { product_name: "Strawberry", product_price: 2.9, product_weight: 620 },
 ];
 
+let to_Check = fruits.product_name;
+
 pNameInput.addEventListener('input', (e) => {
     e.target.classList.remove('is-invalid');
+});
+pPriceInput.addEventListener('input', (e) => {
+    e.target.classList.remove('is-invalid');
+});
+pWeightInput.addEventListener('input', (e) => {
+    e.target.classList.remove('is-invalid');
 })
-
 function addProduct(e) {
+    
+
     e.preventDefault();
     console.log(e);
     let len = fruits.length;
@@ -27,13 +38,43 @@ function addProduct(e) {
         product_price: +e.target[1].value,
         product_weight: +e.target[2].value,
     };
-
-    if (newProduct.product_name === ''
-        && newProduct.product_price === 0
-        && newProduct.product_weight === 0) {
+    if(newProduct.product_name === ''
+    && newProduct.product_price === 0
+    && newProduct.product_weight === 0
+    ){
         e.target[0].classList.add('is-invalid');
-    } else {
+        e.target[1].classList.add('is-invalid');
+        e.target[2].classList.add('is-invalid');
+        
+    }
+    else if(newProduct.product_price === 0
+        && newProduct.product_weight === 0){
+        e.target[1].classList.add('is-invalid');
+        e.target[2].classList.add('is-invalid');
+        }
+    else if(newProduct.product_name === ''
+            && newProduct.product_weight === 0){
+            e.target[0].classList.add('is-invalid');
+            e.target[2].classList.add('is-invalid');
+            }
+    else if(newProduct.product_name === ''
+                && newProduct.product_price === 0){
+                e.target[0].classList.add('is-invalid');
+                e.target[1].classList.add('is-invalid');
+            }
+    else if(newProduct.product_name === ''){
+            e.target[0].classList.add('is-invalid');
+    }else if(newProduct.product_price === 0){
+            e.target[1].classList.add('is-invalid');
+    }else if(newProduct.product_weight === 0){
+            e.target[2].classList.add('is-invalid');
+    }else if(to_Check = e.target[0].value){
+        alert("Error. This fruit is in catalog. Try another fruit.")
+    }
+    else {
         e.target[0].classList.remove('is-invalid');
+        e.target[1].classList.remove('is-invalid');
+        e.target[2].classList.remove('is-invalid');
         fruits.push(newProduct);
 
         let { product_name, product_price, product_weight } = newProduct;
@@ -64,5 +105,7 @@ function writeToTable() {
         </tr>`
     }
 }
+
+
 
 writeToTable();
